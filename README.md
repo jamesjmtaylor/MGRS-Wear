c272e6b74999cc4196c4084852d08468c979cece
James Taylor <james.jm.taylor@gmail.com>
2018-11-04 11:46:59 -0800

    Updated README.

---
+++ README.md
@@ -0,0 +1,7 @@
+# MGRS Wear
+
+This is an Android Wear watch app that displays your current position using the Military Grid Reference System.  The idea is that you generally wear only wear one watch (unless you want to look like a crazy person).  That meant that in the Army you have to choose between an Android Wear device and the Garmin Foretrex wrist GPS.  This application allows you to have the best of both worlds
+
+## Libraries
+
+* None yet.
--- app/src/main/java/com/jamesjmtaylor/mgrswear/MgrsActivity.kt
+++ app/src/main/java/com/jamesjmtaylor/mgrswear/MgrsActivity.kt
@@ -1,15 +1,25 @@
 package com.jamesjmtaylor.mgrswear
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.support.wearable.activity.WearableActivity
+import android.util.Log
 
 class MgrsActivity : WearableActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mgrs)
-
+        if (!hasGps()) {
+            Log.d(TAG, "This hardware doesn't have GPS.")
+            // Fall back to functionality that does not use location or
+            // warn the user that location function is not available.
+        }
         // Enables Always-on
         setAmbientEnabled()
     }
+
+    private fun hasGps(): Boolean =
+        packageManager.hasSystemFeature(PackageManager.FEATURE_LOCATION_GPS)
 }
+private const val TAG = "MGRSactivity"
